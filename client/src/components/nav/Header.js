53748bb0ea311e68b3fd64278a4b1bb0bec9f51a
Naman <snaman87@gmail.com>
2021-03-21 00:49:29 +0530

    Initial Commit adding Ant design

---
+++ client/src/components/nav/Header.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+const Header = () =>{
+    const [state, setState] = useState('')
+    
+}
+export default Header;
